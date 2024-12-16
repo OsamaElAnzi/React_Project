99e1291f010edeea91e7f6a488ab960b276a6a34
osama <129272439+OsamaElAnzi@users.noreply.github.com>
2024-12-16 13:56:32 +0100

    projecten is klaat

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,101 +1,16 @@
-import React, { useCallback } from "react";
-import Particles from "react-tsparticles";
-import { loadSlim } from "tsparticles-slim";
+import React from "react";
 import Navigation from "./componenten/Navigation";
 import Home from "./componenten/Pages/Home";
-import FooterCom from "./componenten/Pages/FooterCom";
 import logo from "./componenten/Img/osama.png";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Projecten from "./componenten/Pages/Projecten";
+import PackageBg from "./componenten/PackageBg";
 
 const App = () => {
-    const particlesInit = useCallback(async (engine) => {
-        console.log(engine);
-        await loadSlim(engine);
-    }, []);
-
-    const particlesLoaded = useCallback(async (container) => {
-        console.log(container);
-    }, []);
-
     return (
         <div className="app-container">
-            <Particles
-                id="tsparticles"
-                init={particlesInit}
-                loaded={particlesLoaded}
-                options={{
-                    background: {
-                        color: {
-                            value: "#0d47a1",
-                        },
-                    },
-                    fpsLimit: 120,
-                    interactivity: {
-                        events: {
-                            onClick: {
-                                enable: true,
-                                mode: "push",
-                            },
-                            onHover: {
-                                enable: true,
-                                mode: "repulse",
-                            },
-                            resize: true,
-                        },
-                        modes: {
-                            push: {
-                                quantity: 4,
-                            },
-                            repulse: {
-                                distance: 150,
-                                duration: 0.4,
-                            },
-                        },
-                    },
-                    particles: {
-                        color: {
-                            value: "#ffffff",
-                        },
-                        links: {
-                            color: "#ffffff",
-                            distance: 150,
-                            enable: true,
-                            opacity: 0.5,
-                            width: 1,
-                        },
-                        move: {
-                            direction: "none",
-                            enable: true,
-                            outModes: {
-                                default: "bounce",
-                            },
-                            random: false,
-                            speed: 6,
-                            straight: false,
-                        },
-                        number: {
-                            density: {
-                                enable: true,
-                                area: 500,
-                            },
-                            value: 80,
-                        },
-                        opacity: {
-                            value: 0.5,
-                        },
-                        shape: {
-                            type: "circle",
-                        },
-                        size: {
-                            value: { min: 1, max: 5 },
-                        },
-                    },
-                    detectRetina: true,
-                }}
-            />
-
+            <PackageBg />
             <Navigation />
             <div className="content-container d-flex flex-row">
                 <Projecten />
